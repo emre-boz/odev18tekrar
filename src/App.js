830828ecb92e2d653b16aa146d246c500e1eb627
Emre Boz <emrento@gmail.com>
2022-10-20 10:49:14 +0300

    reminder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import data from "./data";
+const veriler=data;
 function App(){
+
+  const [veri,setVeri]=useState(veriler);
+
+
+
+
   return (
     <>
     <h1>ben appdullah size nasıl yardımcı olabilirim</h1>
+    <div>
+    {veri&&veri.map((item)=>{return <p key={item.id}>{item.name}</p>})}
+    </div>
+    <div>
+      <button onClick={()=>{setVeri([])}}>hepsini sil</button>
+    </div>
+    
     </>
   )
 }
