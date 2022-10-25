19eed99eae725d6e2b1fc7753e03801868d3c961
Emre Boz <emrento@gmail.com>
2022-10-25 21:29:34 +0300

    accordion-start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useRef, useEffect } from "react";
+import questions from "./dataSorular";
+
 function App(){
+
+
+const [veriler,setVeri]=useState(questions);
+
+
+
+
+
+
+
+
   return (
     <>
     <h1>ben appdullah size nasıl yardımcı olabilirim</h1>
+    {veriler.map((item)=><div key={item.id}>
+     <p>{item.title}</p>
+     
+     <button>cevup</button>
+     <p>{item.info}</p> 
+ 
+     
+
+
+     </div>)}
     </>
   )
 }
