63423077bbecafbcfae09dde1becf97fad12f8d0
Emre Boz <emrento@gmail.com>
2022-11-04 22:23:32 +0300

    bir takım uğraşlar olumsuz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Kompo from "./Kompo";
+
 function App(){
+ const [loading,setLoading]=useState(true);
+  const [data,setData]=useState([]);
+
+const [goster,setGoster]=useState(false);
+
+  const veriCek=async()=>{
+      const fetcher=await fetch('https://course-api.com/react-tabs-project');
+      const jsonFive=await fetcher.json();
+      setLoading(false);
+      setData(jsonFive);
+  }
+
+  useEffect(()=>{
+        veriCek();
+  },[])
+
+  if(loading){
+      return <h1>loading..</h1>
+  }
+
+
   return (
     <>
     <h1>ben appdullah size nasıl yardımcı olabilirim</h1>
+    {data&&data.map((item)=><div>
+      <button onClick={()=>setGoster(!goster)}>{item.company}</button>
+    <Kompo item={item} goster={goster}/>
+      </div>)}
+      <div></div>
     </>
   )
 }
---
+++ src/Kompo.js
@@ -0,0 +1,8 @@
+function Kompo({item,goster}){
+    return(<>
+    
+    <p>{goster?`${item.duties}`:""}</p>
+    </>)
+}
+
+export default Kompo;
