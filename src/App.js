0de8881656aa0dfa9231eeef928fc4327b4fca45
Emre Boz <emrento@gmail.com>
2022-11-06 23:02:11 +0300

    bi takım başlangıçlar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import reviews from "./data";
+
 function App(){
+  const veriler=reviews;
+  const [data,setData]=useState(veriler);
+  const [deger,setDeger]=useState(0);
+console.log("deger",deger);
+console.log("length",data.length);
+
+const arttir=()=>{
+if(deger==data.length-1){
+  return deger=0;
+}else{
+  return deger+1;
+}
+}
+console.log("fonk sonrası deger",deger);
+
   return (
     <>
     <h1>ben appdullah size nasıl yardımcı olabilirim</h1>
+    {data.map((item,index)=><p>{item.name}</p>)}
+
+
+    <button>geri</button>{data[deger].name}<button onClick={()=>setDeger(arttir)}>ileri</button>
     </>
   )
 }
